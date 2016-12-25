cb1956cac0b7923366a3099dfb72ef6c888d8b0d
desprid-zh <desprid-zh@yahoo.com>
2016-12-25 20:22:03 +0800

    update list

--- src/pages/Demo1.vue
+++ src/pages/Demo1.vue
@@ -51,7 +51,81 @@
         </md-tab>
 
         <md-tab class="api-tab" md-label="Classes">
-          <div>tttttt</div>
+<div class="phone-viewport">
+  <md-list class="custom-list md-triple-line">
+    <md-list-item>
+      <md-avatar>
+        <img src="assets/images/avatar-2.jpg" alt="People">
+      </md-avatar>
+
+      <div class="md-list-text-container">
+        <span>Ali Connors</span>
+        <span>Brunch this weekend?</span>
+        <p>I'll be in your neighborhood doing errands...</p>
+      </div>
+
+      <md-button class="md-icon-button md-list-action">
+        <md-icon class="md-primary">star</md-icon>
+      </md-button>
+
+      <md-divider class="md-inset"></md-divider>
+    </md-list-item>
+
+    <md-list-item>
+      <md-avatar>
+        <img src="assets/images/avatar-2.jpg" alt="People">
+      </md-avatar>
+
+      <div class="md-list-text-container">
+        <span>me, Scott, Jennifer</span>
+        <span>Summer BBQ</span>
+        <p>Wish I could come, but I'm out of town ...</p>
+      </div>
+
+      <md-button class="md-icon-button md-list-action">
+        <md-icon>star_border</md-icon>
+      </md-button>
+
+      <md-divider class="md-inset"></md-divider>
+    </md-list-item>
+
+    <md-list-item>
+      <md-avatar>
+        <img src="assets/images/avatar-2.jpg" alt="People">
+      </md-avatar>
+
+      <div class="md-list-text-container">
+        <span>Sandra Adams</span>
+        <span>Oui oui</span>
+        <p>Do you have Paris recommendations ...</p>
+      </div>
+
+      <md-button class="md-icon-button md-list-action">
+        <md-icon>star_border</md-icon>
+      </md-button>
+
+      <md-divider class="md-inset"></md-divider>
+    </md-list-item>
+
+    <md-list-item>
+      <md-avatar>
+        <img src="assets/images/avatar-2.jpg" alt="People">
+      </md-avatar>
+
+      <div class="md-list-text-container">
+        <span>Trevor Hansen</span>
+        <span>Order confirmation</span>
+        <p>Thank you for your recent order from ...</p>
+      </div>
+
+      <md-button class="md-icon-button md-list-action">
+        <md-icon>star_border</md-icon>
+      </md-button>
+
+      <md-divider class="md-inset"></md-divider>
+    </md-list-item>
+  </md-list>
+</div>
         </md-tab>
 
         <md-tab class="api-tab" md-label="Events" >
