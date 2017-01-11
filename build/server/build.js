4061e8aaa8a33d73cd34cd83e7ba0ee1846d5568
desprid-zh <desprid-zh@yahoo.com>
2017-01-11 09:18:35 +0800

    update build script

--- build/server/build.js
+++ build/server/build.js
@@ -1,7 +1,12 @@
+import path from 'path';
 import webpack from 'webpack';
 import webpackConfig from '../webpack/prod';
 import ora from 'ora';
+import fs from 'fs-extra';
+import config from '../config';
 
+const appPath = path.join(config.rootPath, config.appPath);
+// prepare the spinner fake icon
 const spinner = ora({
   text: 'Building...',
   spinner: 'circleQuarters',
@@ -25,4 +30,22 @@ let done = function(error, stats) {
 
   spinner.stop();
 }
+// prepare a clean dist content
+let deleteFolderRecursive = function(path) {
+    var files = [];
+    if( fs.existsSync(path) ) {
+        files = fs.readdirSync(path);
+        files.forEach(function(file,index){
+            var curPath = path + "/" + file;
+            if(fs.statSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
+deleteFolderRecursive(appPath);
+// build the output of app
 webpack(webpackConfig, done);
